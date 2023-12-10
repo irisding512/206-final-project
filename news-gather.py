a6b2ce4604d7c7df60ae92c8e53ebcf037b371cc
Iris Ding <irisding@umich.edu>
2023-12-09 20:07:30 -0500

    changes rankings to be accurate for duplicate data

--- news-gather.py
+++ news-gather.py
@@ -9,22 +9,22 @@ db_name = "countryImpact.db"
 base_url = 'https://newsdata.io/api/1/news?apikey=pub_34479d289637b0172bf42c8842ddfc21776a8&q=covid'
 
 def setUpTable(db_name):
-    # connectToDatabase
+    # connect to database
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
 
-    # createTopStoriesTable
+    # create top stories table
     cur.execute("CREATE TABLE IF NOT EXISTS top_stories(ranking INTEGER, title TEXT PRIMARY KEY, country TEXT, link TEXT)")
     conn.commit()
     return cur, conn
 
 def insertNewsData(cur, conn, ranking, title, country, link):
-    # newsdata.io
+    # newsdata.io API
     # search by keyword "Covid"
     # 10 articles at a time
 
-    # addTopStories
+    # add top stories into database
     cur.execute("INSERT OR IGNORE INTO top_stories (ranking, title, country, link) VALUES (?,?,?,?)", (ranking, title, country, link))
     conn.commit()
 
@@ -37,11 +37,18 @@ if response.status_code == 200:
     stories = data.get('results', [])
     print(stories)
 
-    for index in range(len(stories)):
-        ranking = index + 1
+    for index in range(10):
+        #check how many rows are in table, in case last value was not inserted b/c it was a duplicate, to get rankings
+        cur.execute(f"SELECT COUNT(*) FROM top_stories")
+        row_count = cur.fetchone()[0]
+
+        #assign table variables
+        ranking = row_count + 1
         title = stories[index]['title']
         country = stories[index]['country'][0].title()
         link = stories[index]['link']
+
+        #insert into table
         insertNewsData(cur, conn, ranking, title, country, link)
         print(f"Data for {country} inserted into the database.")
 else:
@@ -49,3 +56,4 @@ else:
     print("Response content:", response.text)
 
 conn.close()
+
