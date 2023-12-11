7e1482946ac27b2f362ec1b9bd645e824bcfd904
Iris Ding <irisding@umich.edu>
2023-12-11 15:31:55 -0500

    added calculations, doesn't write to file yet

--- calculations.py
+++ calculations.py
@@ -5,46 +5,14 @@ import os
 import matplotlib.pyplot as plt
 import requests
 
-db_name = "countryImpact.db"
-base_url = "https://newsdata.io/api/1/news?apikey=pub_34479d289637b0172bf42c8842ddfc21776a8&qInTitle=health"
-
-#country counts in news stories
-
-def setUpCountryCountsTable(db_name):
-    # connect to database
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_name)
-    cur = conn.cursor()
-
-    cur.execute("CREATE TABLE IF NOT EXISTS top_countries_in_stories(country_id INTEGER PRIMARY KEY, count INTEGER)")
-    conn.commit()
-    return cur, conn
-
-
-def countCountries(cur, conn):
-    # count how many times each country appears in the top_stories table
-    cur.execute("SELECT country_id, COUNT(country_id) FROM latest_stories GROUP BY country_id ORDER BY COUNT(country_id) DESC")
-    country_counts = cur.fetchall()
-
-    # print the results
-    for country, count in country_counts:
-        print(f"{country}: {count} times")
-        cur.execute("INSERT OR IGNORE INTO top_countries_in_stories (country_id, count) VALUES (?,?)", (country, count))
-    conn.commit()
-
-cur, conn = setUpCountryCountsTable(db_name)
-countCountries(cur, conn)
-conn.close()
-
-PUTTING THIS OVER HERE FOR NOW!
-
-
-# Perform calculations for recovery percentage
-# Create a table named "recoveryPercent" if it doesn't exist
+# Set up database connection
 db_name = "countryImpact.db"
 path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+db_name)
 cur = conn.cursor()
+
+# PART ONE: Perform calculations for recovery percentage
+# Create a table named "recoveryPercent" if it doesn't exist
 cur.execute('''
     CREATE TABLE IF NOT EXISTS recoveryPercent (
         country_id INTEGER PRIMARY KEY,
@@ -62,18 +30,42 @@ cur.execute('''
     JOIN countryKeys ck ON cd.country_id = ck.country_id
 ''')
 
+conn.commit()
 results = cur.fetchall()
 
 # Write calculated data to a text file
-output_file_path = "recovery_percentage_data.txt"
+output_file_path = "calculations.txt"
 with open(output_file_path, "w") as output_file:
     for result in results:
         country_id, cases, recovered, recovery_percentage = result
         output_file.write(f"Country ID: {country_id}, Cases: {cases}, Recovered: {recovered}, Recovery Percentage: {recovery_percentage:.2f}%\n")
 
 
-# Commit the changes
+
+#PART TWO: CALCULATE COUNTRY COUNTS
+base_url = "https://newsdata.io/api/1/news?apikey=pub_34479d289637b0172bf42c8842ddfc21776a8&qInTitle=health"
+
+#create country counts table
+cur.execute("CREATE TABLE IF NOT EXISTS top_countries_in_stories(country_id INTEGER PRIMARY KEY, count INTEGER)")
+
+# count how many times each country appears in the top_stories table
+cur.execute("SELECT country_id, COUNT(country_id) FROM latest_stories GROUP BY country_id ORDER BY COUNT(country_id) DESC")
+country_counts = cur.fetchall()
+
+# print the results
+for country_id, count in country_counts:
+    print(f"{country_id}: {count} times")
+    cur.execute("INSERT OR IGNORE INTO top_countries_in_stories (country_id, count) VALUES (?,?)", (country_id, count))
+
 conn.commit()
+results = cur.fetchall()
+
+# Write calculated data to a text file
+output_file_path = "calculations.txt"
+with open(output_file_path, "w") as output_file:
+    for result in results:
+        country_id, count = result
+        output_file.write(f"Country ID: {country_id}, Count: {count}%\n")
 
 # Close the connection
 conn.close()
